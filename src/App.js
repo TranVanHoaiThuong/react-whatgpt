77ef9fd7bfa5a6e800b1e1b6d6634bc59ebd579c
TranVanHoaiThuong <thuong10t2678@gmail.com>
2024-09-17 19:20:28 +0700

    Full project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import ScrollToTop from "react-scroll-to-top";
+import "./App.css";
+import { Brand, CTA, Navbar } from "./components";
+import { RiArrowUpFill } from "react-icons/ri";
+import {
+    Blog,
+    Features,
+    Footer,
+    Header,
+    Possibility,
+    WhatGPT3,
+} from "./containers";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar />
+                <Header />
+            </div>
+            <Brand />
+            <WhatGPT3 />
+            <Features />
+            <Possibility />
+            <CTA />
+            <Blog />
+            <Footer />
+            <ScrollToTop
+                smooth
+                style={{ borderRadius: "50%", background: "#fff" }}
+                width="20"
+                height="20"
+                component={<RiArrowUpFill size={20} />}
+            />
+        </div>
+    );
 }
 
 export default App;
